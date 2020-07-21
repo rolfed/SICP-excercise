7a1dbde8cdb27e019fed99bdc4ba8d1f2ae6df5a
s004057 <danniel.rolfe@standard.com>
2020-07-21 13:44:18 -0700

    Add ex 1.12

--- ch_1/ex_1.12.scm
+++ ch_1/ex_1.12.scm
@@ -1,3 +1,4 @@
-(define (pascal-triangle a b n)
-  (if (= n 0) 1
-    else ((+ a b)))
+(define (pascal row column)
+  (if (or (= column 1) (= column row))
+    1
+    (+ (pascal (- row 1) (- column 1) (pascal (- row 1) column)))))
